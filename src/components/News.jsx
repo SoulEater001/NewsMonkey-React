71eefdf3561a3206b27f22f0284556ec51e682ce
SoulEater001 <dshivam698@gmail.com>
2024-12-02 01:25:12 +0530

    Fetch api key and small setup

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,12 +4,23 @@ import NewsItem from './NewsItem'
 export class News extends Component {
     render() {
         return (
-            <div>
-                This is news component.
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
+            <div className='container my-3'>
+                <h2>NewsMonkey - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' />
+                    </div>
+                    <div className="col-md-3">
+                        <NewsItem title='myTitle' description='myDescription' />
+                    </div>
+                </div>
+
             </div>
         )
     }
